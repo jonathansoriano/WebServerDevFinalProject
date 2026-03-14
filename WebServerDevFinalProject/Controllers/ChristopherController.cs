0a9c6d11cfbb6429f984e20263d3443b9a6207db
cwagr <agricocw@mail.uc.edu>
2026-03-13 23:31:43 -0400

    First Commit

---
+++ WebServerDevFinalProject/Controllers/ChristopherController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebServerDevFinalProject.Models;
+
+namespace WebServerDevFinalProject.Controllers
+{
+    public class ChristopherController : Controller
+    {
+        private HobbyContext context { get; set; }
+
+        public ChristopherController(HobbyContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult AboutMe()
+        {
+            return View();
+        }
+
+        public IActionResult HobbyList()
+        {
+            var christopherHobbies = context.Christopher
+                .OrderBy(hobby => hobby.HobbyName)
+                .ToList();
+
+            return View(christopherHobbies);
+        }
+    }
+}
