19b4eef088a246c1ba62ddfc169c4de55747db0d
cwagr <agricocw@mail.uc.edu>
2026-03-13 23:43:05 -0400

    Added hobbylist and models

---
+++ WebServerDevFinalProject/Models/Christopher.cs
@@ -0,0 +1,13 @@
+﻿namespace WebServerDevFinalProject.Models
+{
+    public class Christopher
+    {
+        public int ChristopherId { get; set; }
+        public string HobbyName { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Environment { get; set; } = string.Empty;
+        public string SkillLevel { get; set; } = string.Empty;
+        public int HobbyAge { get; set; }
+    }
+}
